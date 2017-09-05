43b768eae7bd93cdb569fea7e9c404e5d35d1a05
Maxim Kulkin <maxim.kulkin@gmail.com>
2017-09-05 16:50:17 -0700

    Bump version to 0.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name='lollipop-jsonschema',
-    version='0.5',
+    version='0.6',
     description=('Library to convert Lollipop schema to JSON schema'),
     long_description=read('README.rst'),
     author='Maxim Kulkin',
